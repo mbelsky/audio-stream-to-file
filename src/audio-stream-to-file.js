2440978cd8365dda6bc2b45b24c5ed21f48f0661
Max Belsky <public.belsky@gmail.com>
2022-04-10 02:52:47 +0200

    Set tryToGetStreamResponse time out

--- src/audio-stream-to-file.js
+++ src/audio-stream-to-file.js
@@ -1,11 +1,15 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import {URL} from 'url'
+import {logger} from './logger.js'
 import {
   isDestDirOk,
   saveAudioStreamToFile,
 } from './save-audio-stream-to-file.js'
 import {tryToGetStreamResponse} from './try-to-get-stream-response.js'
+
+const GET_STREAM_RESPONSE_TIMEOUT = 2 * 60 * 60 * 1000
+
 ;['STREAM_URL', 'DEST_DIR'].forEach((variable) => {
   if (!process.env[variable]) {
     throw new Error(`Variable '${variable}' has to be defined`)
@@ -19,19 +23,42 @@ if (!isDestDirOk(destDirPath)) {
 }
 
 const url = new URL(process.env.STREAM_URL)
-const res = await tryToGetStreamResponse(url)
 
-if (!res) {
-  throw new Error('response object is falsy')
+try {
+  const response = await Promise.race([
+    tryToGetStreamResponse(url),
+    getTimeoutPromise(),
+  ])
+
+  if (!response) {
+    throw new Error('response object is falsy')
+  }
+
+  const {headers} = response
+  const contentType = headers['content-type']
+  const [, audioFormat] = contentType.split('/')
+
+  const filename = new Date().toISOString()
+
+  const writeStreamPath = path.resolve(
+    destDirPath,
+    `${filename}.${audioFormat}`,
+  )
+
+  const writeStream = fs.createWriteStream(writeStreamPath)
+
+  saveAudioStreamToFile(response, writeStream)
+} catch {
+  // well we've tried.
+  process.exit(0)
 }
 
-const {headers} = res
-const contentType = headers['content-type']
-const [, audioFormat] = contentType.split('/')
-const filename = new Date().toISOString()
+function getTimeoutPromise() {
+  return new Promise((_resolve, reject) => {
+    setTimeout(() => {
+      logger.error('tryToGetStreamResponse time out')
 
-const writeStreamPath = path.resolve(destDirPath, `${filename}.${audioFormat}`)
-
-const writeStream = fs.createWriteStream(writeStreamPath)
-
-saveAudioStreamToFile(res, writeStream)
+      reject()
+    }, GET_STREAM_RESPONSE_TIMEOUT)
+  })
+}
