175c96d082878f5972cbd0f36ebf65bcb340e970
Max Belsky <public.belsky@gmail.com>
2022-07-30 16:52:53 +0200

    Add attempt mechanism

--- src/audio-stream-to-file.js
+++ src/audio-stream-to-file.js
@@ -9,7 +9,7 @@ import {
 } from './save-audio-stream-to-file.js'
 import {tryToGetStreamResponse} from './try-to-get-stream-response.js'
 
-const GET_STREAM_RESPONSE_TIMEOUT = 2 * 60 * 60 * 1000
+const GET_STREAM_RESPONSE_TIMEOUT = 1.5 * 60 * 60 * 1000
 
 ;['STREAM_URL', 'DEST_DIR'].forEach((variable) => {
   if (!process.env[variable]) {
@@ -18,42 +18,53 @@ const GET_STREAM_RESPONSE_TIMEOUT = 2 * 60 * 60 * 1000
 })
 
 const destDirPath = path.resolve(process.cwd(), process.env.DEST_DIR)
-
-if (!isDestDirOk(destDirPath)) {
-  throw new Error('dest dir is not valid')
-}
-
 const url = new URL(process.env.STREAM_URL)
 
-let response
+run()
 
-try {
-  const [timeoutPromise, timeoutPromiseId] = getTimeoutPromise()
+async function run(attempt = 0) {
+  logger.log(`run #${attempt}`)
 
-  response = await Promise.race([tryToGetStreamResponse(url), timeoutPromise])
+  if (!isDestDirOk(destDirPath)) {
+    throw new Error('dest dir is not valid')
+  }
 
-  clearTimeout(timeoutPromiseId)
-} catch (error) {
-  logger.error(error)
-  // After time out finish as a successful process
-  process.exit()
+  let response
+
+  try {
+    const [timeoutPromise, timeoutPromiseId] = getTimeoutPromise()
+
+    response = await Promise.race([tryToGetStreamResponse(url), timeoutPromise])
+
+    clearTimeout(timeoutPromiseId)
+  } catch (error) {
+    logger.error(error)
+    // After time out finish as a successful process
+    process.exit()
+    // Any succesful run should end here
+  }
+
+  if (!response) {
+    throw new Error('response object is falsy')
+  }
+
+  const {headers} = response
+  const contentType = headers['content-type']
+  const [, audioFormat] = contentType.split('/')
+
+  const filename = generateFilename(new Date().toISOString(), audioFormat)
+
+  logger.log(`Resolve ${filename} path and create write stream`)
+
+  const writeStreamPath = path.resolve(destDirPath, filename)
+
+  const writeStream = fs.createWriteStream(writeStreamPath)
+
+  await saveAudioStreamToFile(response, writeStream)
+
+  // After successful saveAudioStreamToFile execute `run` again in case stream is restarting.
+  run(attempt + 1)
 }
-
-if (!response) {
-  throw new Error('response object is falsy')
-}
-
-const {headers} = response
-const contentType = headers['content-type']
-const [, audioFormat] = contentType.split('/')
-
-const filename = generateFilename(new Date().toISOString(), audioFormat)
-
-const writeStreamPath = path.resolve(destDirPath, filename)
-
-const writeStream = fs.createWriteStream(writeStreamPath)
-
-saveAudioStreamToFile(response, writeStream)
 
 function getTimeoutPromise() {
   let id
